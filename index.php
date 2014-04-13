8148cabfc1d35e1361ee8468b14e543c138c80cd
devmount <hpdesigner@web.de>
2014-04-13 23:16:26 +0200

    fixed: deprecated link and license

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
  * @category PHP
  * @package  PHP_MoziloPlugins
  * @author   HPdesigner <mail@devmount.de>
- * @license  GPL v3
+ * @license  GPL v3+
  * @version  GIT: v1.1.2013-09-19
  * @link     https://github.com/devmount/quickComment
- * @link     http://devmount.de/Develop/Mozilo%20Plugins/quickComment.html
+ * @link     http://devmount.de/Develop/moziloCMS/Plugins/quickComment.html
  * @see      I can do all this through him who gives me strength.
  *           – The Bible
  *
@@ -33,7 +33,7 @@ if (!defined('IS_CMS')) {
  * @category PHP
  * @package  PHP_MoziloPlugins
  * @author   HPdesigner <mail@devmount.de>
- * @license  GPL v3
+ * @license  GPL v3+
  * @link     https://github.com/devmount/quickComment
  */
 class quickComment extends Plugin
